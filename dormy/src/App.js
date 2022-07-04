4d96277f018bb2ab32d30607470aa7e105a92b61
Param Nigam <pqn5106@psu.edu>
2022-07-04 18:04:45 -0400

    Initial Commit

---
+++ dormy/src/App.js
@@ -0,0 +1,13 @@
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Onboarding from './pages/Onboarding'
+
+const App = () => {
+  return (
+    <div >
+
+    </div>
+  );
+}
+
+export default App;
